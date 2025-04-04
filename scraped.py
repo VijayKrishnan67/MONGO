889c8ff785c506e79c6409bad3c52ca3f1f42bab
VijayKrishnan67 <152212556+VijayKrishnan67@users.noreply.github.com>
2025-04-04 14:57:45 +0530

    Update scraped.py

--- scraped.py
+++ scraped.py
@@ -24,10 +24,12 @@ def connect_mongo():
 
 def scrape_tradingview():
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")  # Use new headless mode
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920x1080")
+
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     url = "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/"
